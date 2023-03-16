3728eef8c087d0e9c7692f139e7e2679d70e0178
MarcB77 <59608472+MarcB77@users.noreply.github.com>
2023-03-16 16:52:10 +0100

    update spinner

--- analysis_app.py
+++ analysis_app.py
@@ -26,41 +26,39 @@ selected_sport = st.multiselect("Selecteer een type sport:",
                )
 
 with st.spinner("Just a moment ..."):
-    time.sleep(1)
+    if selected_sport != []:
+        fig = plt.figure(figsize=(12,15))
+        ax1 = fig.add_subplot(5, 1, 1)
+        ax2 = fig.add_subplot(5, 1, 2)
+        ax3 = fig.add_subplot(5, 1, 3)
+        ax4 = fig.add_subplot(5, 1, 4)
+        ax5 = fig.add_subplot(5, 1, 5)
+        plt.subplots_adjust(hspace=0.5)
+        sns.histplot(
+            df.loc[df.Type_sport == selected_sport[0]], x='word_count', kde=True, 
+            color="#100c44", binwidth = 1, alpha = 0.7, ax=ax1
+            )
+        ax1.set_title('Total amount of words')
 
-if selected_sport != []:
-    fig = plt.figure(figsize=(12,15))
-    ax1 = fig.add_subplot(5, 1, 1)
-    ax2 = fig.add_subplot(5, 1, 2)
-    ax3 = fig.add_subplot(5, 1, 3)
-    ax4 = fig.add_subplot(5, 1, 4)
-    ax5 = fig.add_subplot(5, 1, 5)
-    plt.subplots_adjust(hspace=0.5)
-    sns.histplot(
-        df.loc[df.Type_sport == selected_sport[0]], x='word_count', kde=True, 
-        color="#100c44", binwidth = 1, alpha = 0.7, ax=ax1
+        corpus = get_corpus(df)
+        words, freq = most_common_words(corpus)
+        sns.barplot(x=freq, y=words, color="#100c44", ax=ax2)
+        ax2.set_title('Top 10 Most Frequently Occuring Words')
+
+        wordcloud= WordCloud(max_font_size=60, max_words=100,width=1000,height=200, stopwords=STOPWORDS, background_color='#FFFFFF').generate_from_frequencies(
+        FreqDist([word for prompt in df.Prompt_lists for word in prompt])
         )
-    ax1.set_title('Total amount of words')
+        ax3.imshow(wordcloud)
+        ax3.grid(visible=False)
+        ax3.set_xticks([])
+        ax3.set_yticks([])
 
-    corpus = get_corpus(df)
-    words, freq = most_common_words(corpus)
-    sns.barplot(x=freq, y=words, color="#100c44", ax=ax2)
-    ax2.set_title('Top 10 Most Frequently Occuring Words')
+        ngram_freq, ngram_type = Bigrams(df)
+        sns.barplot(x=ngram_freq['frequency'][:10], y=ngram_freq['ngram'][:10], color="#100c44", ax=ax4)
+        ax4.set_title('Top 10 Most Frequently Occuring {}'.format(ngram_type))
 
-    wordcloud= WordCloud(max_font_size=60, max_words=100,width=1000,height=200, stopwords=STOPWORDS, background_color='#FFFFFF').generate_from_frequencies(
-    FreqDist([word for prompt in df.Prompt_lists for word in prompt])
-    )
-    ax3.imshow(wordcloud)
-    ax3.grid(visible=False)
-    ax3.set_xticks([])
-    ax3.set_yticks([])
+        ngram_freq, ngram_type = Trigrams(df)
+        sns.barplot(x=ngram_freq['frequency'][:10], y=ngram_freq['ngram'][:10], color="#100c44", ax=ax5)
+        ax5.set_title('Top 10 Most Frequently Occuring {}'.format(ngram_type))
 
-    ngram_freq, ngram_type = Bigrams(df)
-    sns.barplot(x=ngram_freq['frequency'][:10], y=ngram_freq['ngram'][:10], color="#100c44", ax=ax4)
-    ax4.set_title('Top 10 Most Frequently Occuring {}'.format(ngram_type))
-
-    ngram_freq, ngram_type = Trigrams(df)
-    sns.barplot(x=ngram_freq['frequency'][:10], y=ngram_freq['ngram'][:10], color="#100c44", ax=ax5)
-    ax5.set_title('Top 10 Most Frequently Occuring {}'.format(ngram_type))
-
-    st.pyplot(fig)
+        st.pyplot(fig)
